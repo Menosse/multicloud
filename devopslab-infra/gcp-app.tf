68905bff1270a961589698ba2d18116530606679
Fernando Takada <fernando.b.takada@rakuten.com>
2021-11-06 22:26:30 +0900

    try app engine again

--- devopslab-infra/gcp-app.tf
+++ devopslab-infra/gcp-app.tf
@@ -1,5 +1,5 @@
-resource "google_app_engine_application" "app" {
-  project     = "multicloud-gcp-app"
+resource "google_app_engine_application" "multicloud-gcp-app" {
+  project     = "lab-cloud-devops-01"
   location_id = "us-central"
-  
+
 }
